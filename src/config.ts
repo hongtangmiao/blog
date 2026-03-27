7f753fd1f0c844b6af9b1af4a30b8f52247710a8
hongtangmiao <hongtangmiao@users.noreply.github.com>
2026-03-27 18:10:13 +0800

    Update favicon and banner credit

--- src/config.ts
+++ src/config.ts
@@ -20,9 +20,9 @@ export const siteConfig: SiteConfig = {
 		src: "/assets/images/LUO.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
-			url: "", // (Optional) URL link to the original artwork or artist's page
+			enable: true, // Display the credit text of the banner image
+			text: "洛天依 上海禾念信息科技有限公司", // Credit text to be displayed
+			url: "https://www.bilibili.com/opus/919456836300570643", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	toc: {
@@ -30,12 +30,10 @@ export const siteConfig: SiteConfig = {
 		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
 	},
 	favicon: [
-		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
+		{
+			src: '/assets/images/avatar.png',
+			sizes: 'any',
+		},
 	],
 };
 
